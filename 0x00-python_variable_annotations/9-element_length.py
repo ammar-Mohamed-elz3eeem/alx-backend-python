ce4434863ed27dc867843cf00160c8f31dcd31d9
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-05 12:02:41 +0200

    add the element_length type annonations

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" module that define sum_mixed_list function
+"""
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
