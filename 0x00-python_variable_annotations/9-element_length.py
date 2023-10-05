f14b59c2f01050e406cde0799f6856f9596c2103
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-05 12:05:44 +0200

    add function documentation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-""" module that define sum_mixed_list function
+"""module that define sum_mixed_list function
 """
 from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """determine the length of every list in the multidimensional lst
+
+    Args:
+        lst (Iterable[Sequence]): multidimensional list
+
+    Returns:
+        List[Tuple[Sequence, int]]: list of tuples that contains
+        every list on the multidimensional list
+        and the length of that list
+    """
     return [(i, len(i)) for i in lst]
