5e25f03e0d9adaccc9b14fb97fb3bf232ca0c812
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-09 23:29:02 +0200

    add function documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,16 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 0):
-    random_num = random.random() * 15
+async def wait_random(max_delay: int = 0) -> float:
+    """Asyncronsly make random number and return it
+
+    Args:
+        max_delay (int): max number that the programm
+        will have to wait. Defaults to 0.
+
+    Returns:
+        float: random number that has been generated
+    """
+    random_num = random.random() * max_delay
     await asyncio.sleep(random_num)
     return random_num
