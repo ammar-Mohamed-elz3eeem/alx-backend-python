d8ed81e3c19dc1cd5316cb95094686c1f6d56bb0
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-10 00:06:31 +0200

    add measure_time functionality

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""wait_random functionality module
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 0) -> float:
+    current = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.perf_counter() - current) / n
