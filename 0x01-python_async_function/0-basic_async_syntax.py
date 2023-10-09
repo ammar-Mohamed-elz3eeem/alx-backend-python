b48c426925665a781f0fe3dd2a06b528e4bbe24b
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-09 23:22:36 +0200

    basic anync

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""wait_random functionality module
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay = 0):
+    random_num = random.random() * 15
+    asyncio.sleep(random_num)
+    return random_num
