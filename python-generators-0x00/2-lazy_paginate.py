d2a8c6a884ba27f8b50733933879fcec458b498e
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2024-12-08 03:04:38 +0200

    Use paginate_users function inside lazy paginate

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,5 +1,25 @@
-from seed import connect_to_prodev, User
-from typing import Generator, Tuple
+import seed
+from typing import Generator, Tuple, List
+
+
+def paginate_users(page_size: int, offset: int) -> List[Tuple[str, ...]]:
+    """ This function returns number of users from database equal to
+        page_size at the given offset
+
+    Args:
+        page_size (int): This is maximum number of users to return
+        offset (int): This is the offset to get start count users from
+
+    Returns:
+        List[Tuple[str, ...]]: List of all users
+    """
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(
+        f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
 
 
 def lazy_paginate(page_size: int) -> Generator[Tuple[str, ...], None, None]:
@@ -13,18 +33,15 @@ def lazy_paginate(page_size: int) -> Generator[Tuple[str, ...], None, None]:
         Generator: The users from database in chunks.
     """
     offset = 0
-    connection = connect_to_prodev()
-    cursor = connection.cursor()
     while True:
         try:
-            cursor.execute(
-                "SELECT * FROM user_data LIMIT %s OFFSET %s", [page_size, offset])
+            result = paginate_users(page_size, offset)
 
-            result = cursor.fetchall()
             if not result:
                 break
 
             yield result
+
             offset += page_size
         except Exception as err:
             print("Failed to read from database", err)
