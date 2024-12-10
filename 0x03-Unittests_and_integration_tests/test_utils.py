aef5ccac5e0a64de3692d8746f8c274d3b144dbe
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2024-12-11 01:36:21 +0200

    remove some tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,11 +18,11 @@ class TestAccessNestedMap(unittest.TestCase):
     """
 
     @parameterized.expand([
-        param("One", {"a": 1}, ("a",), expected=1),
-        param("Dict", {"a": {"b": 2}}, ("a",), expected={"b": 2}),
-        param("Two", {"a": {"b": 2}}, ("a", "b"), expected=2)
+        param({"a": 1}, ("a",), expected=1),
+        param({"a": {"b": 2}}, ("a",), expected={"b": 2}),
+        param({"a": {"b": 2}}, ("a", "b"), expected=2)
     ])
-    def test_access_nested_map(self, name, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, expected):
         """ This is the method that tests the access_nested_map
             and checks that it returns the right data.
 
@@ -61,57 +61,57 @@ class TestAccessNestedMap(unittest.TestCase):
             utils.access_nested_map(nested_map, path)
 
 
-class TestGetJson(unittest.TestCase):
-    """ This is a class to test get_json functionality.
+# class TestGetJson(unittest.TestCase):
+#     """ This is a class to test get_json functionality.
 
-    Args:
-        unittest (unittest.TestCase)
-            This is the base class for testing get_json().
-    """
+#     Args:
+#         unittest (unittest.TestCase)
+#             This is the base class for testing get_json().
+#     """
 
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    def test_get_json(self, url, payload):
-        """ This method tests the get_json functionality and checks
-            that the method has called the json method.
+#     @parameterized.expand([
+#         ("http://example.com", {"payload": True}),
+#         ("http://holberton.io", {"payload": False})
+#     ])
+#     def test_get_json(self, url, payload):
+#         """ This method tests the get_json functionality and checks
+#             that the method has called the json method.
 
-        Args:
-            url (str)
-                This is the url to send to get_json.
+#         Args:
+#             url (str)
+#                 This is the url to send to get_json.
 
-            payload (Dict)
-                This is the payload response coming from the
-                mocked get request.
-        """
-        attrs = {"json.return_value": payload}
-        with unittest.mock.patch("requests.get",
-                                 return_value=Mock(**attrs)) as mock:
-            ret = utils.get_json(url)
-            mock.assert_called_once_with(url)
-            self.assertEqual(ret, payload)
-
-
-class TestMemoize(unittest.TestCase):
-    """ This is a class to test memoize functionality.
-
-    Args:
-        unittest (unittest.TestCase)
-            This is the base class for testing memoize().
-    """
-
-    def test_memoize(self):
-        """ This is a test case for the memoize function
-        """
-        class TestClass:
-            def a_method(self):
-                return 42
-
-            @utils.memoize
-            def a_property(self):
-                return self.a_method()
+#             payload (Dict)
+#                 This is the payload response coming from the
+#                 mocked get request.
+#         """
+#         attrs = {"json.return_value": payload}
+#         with unittest.mock.patch("requests.get",
+#                                  return_value=Mock(**attrs)) as mock:
+#             ret = utils.get_json(url)
+#             mock.assert_called_once_with(url)
+#             self.assertEqual(ret, payload)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# class TestMemoize(unittest.TestCase):
+#     """ This is a class to test memoize functionality.
+
+#     Args:
+#         unittest (unittest.TestCase)
+#             This is the base class for testing memoize().
+#     """
+
+#     def test_memoize(self):
+#         """ This is a test case for the memoize function
+#         """
+#         class TestClass:
+#             def a_method(self):
+#                 return 42
+
+#             @utils.memoize
+#             def a_property(self):
+#                 return self.a_method()
+
+
+# if __name__ == '__main__':
+#     unittest.main()
