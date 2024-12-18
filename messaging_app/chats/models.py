a79fd7e07dbec1b92a0c6832eba61c22719e0bef
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2024-12-18 03:36:21 +0200

    Add message_body to message model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+import uuid
 
 
 class User(AbstractUser):
@@ -9,7 +10,9 @@ class User(AbstractUser):
         ('host', 'Host'),
         ('admin', 'Admin')
     )
-    id = models.UUIDField(db_column='user_id', primary_key=True)
+    id = models.UUIDField(db_column='user_id',
+                          primary_key=True,
+                          default=uuid.uuid4)
     password = models.CharField(db_column='password_hash', max_length=100)
     phone_number = models.CharField(max_length=20)
     role = models.CharField(max_length=10,
@@ -17,18 +20,22 @@ class User(AbstractUser):
                             default='guest')
     date_joined = models.DateTimeField(db_column='created_at',
                                        default=timezone.now)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
     class Meta:
         db_table = 'users'
 
 
 class Message(models.Model):
-    id = models.UUIDField(db_column='message_id', primary_key=True)
+    id = models.UUIDField(db_column='message_id',
+                          primary_key=True,
+                          default=uuid.uuid4)
     sender = models.ForeignKey(User,
                                   on_delete=models.CASCADE,
                                   related_name="messages_sent")
-    receiver = models.ForeignKey(User,
-                                    on_delete=models.CASCADE,
-                                    related_name="messages_received")
+    message_body = models.TextField(null=False)
     sent_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -37,7 +44,8 @@ class Message(models.Model):
 
 class Conversation(models.Model):
     id = models.UUIDField(db_column='conversation_id',
-                          primary_key=True)
+                          primary_key=True,
+                          default=uuid.uuid4)
     participants = models.ForeignKey(User,
                                         on_delete=models.CASCADE,
                                         related_name='conversations')
